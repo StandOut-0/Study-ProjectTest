019b3d16c2f37d1dcefadc93ac34f807d7f0f8eb
StandOut <StandOut.first@gmail.com>
2023-04-27 11:31:48 +0900

    프로젝트 생성

---
+++ src/test/java/com/testproject/TestProjectApplicationTests.java
@@ -0,0 +1,15 @@
+package com.testproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//메인클래스에 선언된 @SpringBootApplication과 비슷한 애너테이션
+//테스트에 필요한 설정,빈들을 자동으로 초기화.
+@SpringBootTest
+class TestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
